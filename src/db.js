ac8b94f2e750826d299cb50eafedfb13da8e7ef9
edarcode <edarcode@gmail.com>
2022-08-13 07:04:56 -0500

    refactor(src/db.js)

--- src/db.js
+++ src/db.js
@@ -1,4 +1,5 @@
-import { Op, Sequelize } from "sequelize";
+import { Op } from "sequelize";
+import { sequelize } from "./config/conexionDb.js";
 import { defineBill } from "./models/Bill.js";
 import { defineCategory } from "./models/Category.js";
 import { defineDetail } from "./models/Detail.js";
@@ -6,35 +7,6 @@ import { defineImage } from "./models/Image.js";
 import { defineProduct } from "./models/Product.js";
 import { defineSupplier } from "./models/Supplier.js";
 import { defineUser } from "./models/User.js";
-
-const sequelize =
-	process.env.NODE_ENV === "production"
-		? new Sequelize({
-				database: process.env.DB_NAME,
-				dialect: "postgres",
-				host: process.env.DB_HOST,
-				port: 5432,
-				username: process.env.DB_USER,
-				password: process.env.DB_PASSWORD,
-				pool: {
-					max: 3,
-					min: 1,
-					idle: 10000
-				},
-				dialectOptions: {
-					ssl: {
-						require: true,
-						// Ref.: https://github.com/brianc/node-postgres/issues/2009
-						rejectUnauthorized: false
-					},
-					keepAlive: true
-				},
-				ssl: true
-		  })
-		: new Sequelize(
-				`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
-				{ logging: false, native: false }
-		  );
 
 defineUser(sequelize);
 defineSupplier(sequelize);
