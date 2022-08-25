bccdbac0bda0cee437015542e3c92f54e6be8336
edarcode <edarcode@gmail.com>
2022-08-25 16:42:18 -0500

    refactor(src/utils/fillProduct.js)

--- src/utils/fillProduct.js
+++ src/utils/fillProduct.js
@@ -2,7 +2,6 @@ import { products } from "../data/products.js";
 import { createProduct } from "./createProduct.js";
 
 export const fillProduct = async () => {
-	console.log(products.length);
 	for (let i = 0; i < products.length; i++) {
 		const element = products[i];
 		await createProduct(element);
